567543fffe8350e0deb4ea8e038aa345253837ce
rjosyula <ramakanth.josyula@commscope.com>
2021-01-03 00:03:40 -0800

    Added new tests for "multply" and "divide"

--- test_Calculator.py
+++ test_Calculator.py
@@ -8,3 +8,10 @@ class TestCalculatorLibrary:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiply(self):
+        assert 100 == calculator.multiply(10, 10)
+
+    def test_divide(self):
+        assert 10 == calculator.divide(100, 10)
+
